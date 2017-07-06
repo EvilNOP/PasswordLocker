3eaaab1949765dfbb5123b12dae2fa6969f2a417
Matthew <evilnops@gmail.com>
2017-07-06 14:29:01 +0800

    Refactored `pw.y` by adding the capability of reading account name from csv file

--- pw.py
+++ pw.py
@@ -1,26 +1,29 @@
 #! python
 # pw.py - An insecure password locker program.
-import sys, pyperclip
+import csv, sys, pyperclip
 
-# Passwords example .
-PASSWORDS = {
-    'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
-    'blog' : 'VmALvQyKAxiVH5G8v01if1MLZF3sdt'
-}
-
-# Exit the program if the sys.argv list has fewer than two values in it(means miss the account).
-if len(sys.argv) < 2:
-    print('Usage: python pw.py [account] - copy account password')
+# Exit the program if the sys.argv list has fewer than three values in it.
+if len(sys.argv) < 3:
+    print('Usage: python pw.py [path of csv file] [account] - copy account password')
 
     sys.exit()
 
-# First command line argv is the account name.
-account = sys.argv[1]
+# First command line argv is the path of csv file.
+csvFilePath = sys.argv[1]
 
-# Copy the right password if the account name exists.
-if account in PASSWORDS:
-    pyperclip.copy(PASSWORDS[account])
+# Second command line argv is the account name.
+account = sys.argv[2]
 
-    print('Password for ' + account + ' copied to clipboard.')
-else:
-    print('There is no account named ' + account)
+with open(csvFilePath) as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    # Iterate over `reader` to copy the right password if the account name exists.
+    for row in reader:
+        if row['username'] == account:
+            pyperclip.copy(row['password'])
+
+            print('Password for ' + account + ' copied to clipboard.')
+
+            break
+    else:
+        print('There is no account named ' + account)
