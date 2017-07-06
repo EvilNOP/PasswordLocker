4f94b65d37c1d854948fc1cf691ec2c39dc28e41
Matthew <evilnops@gmail.com>
2017-07-06 17:13:27 +0800

    Added `accountWriter` function to write account into csv file

--- pw.py
+++ pw.py
@@ -1,6 +1,6 @@
 #! python
 # pw.py - An insecure password locker program.
-import csv, sys, pyperclip
+import os, csv, sys, pyperclip
 
 def passwordReader(path, username, dialect=csv.excel, **kwargs):
     with open(path) as csvFile:
@@ -16,6 +16,18 @@ def passwordReader(path, username, dialect=csv.excel, **kwargs):
                 break
         else:
             print('There is no account named ' + username)
+
+def accountWriter(path, account, dialect=csv.excel, **kwargs):
+    with open(path, 'a') as csvFile:
+        fieldnames = ['username', 'password', 'category']
+
+        writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+
+        # Writer header if the csv file is newly created
+        if os.path.getsize(path) == 0:
+            writer.writeheader()
+
+        writer.writerow({'username': account.username, 'password': account.password, 'category': account.category})
 
 if __name__ == '__main__':
     # Exit the program if the sys.argv list has fewer than three values in it.
